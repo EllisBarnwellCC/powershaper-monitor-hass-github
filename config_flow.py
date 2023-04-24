d764d6582e7442e280bf0de0f483b61fb51ed68b
MemorySounds <r.sahakyan1@gmail.com>
2023-04-24 15:38:44 +0200

    Added initial call to powershaper api

--- config_flow.py
+++ config_flow.py
@@ -2,23 +2,53 @@
 from typing import Any
 from homeassistant import config_entries, exceptions
 from homeassistant.core import HomeAssistant
-from .const import DOMAIN
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from .const import (DOMAIN, API_TOKEN_LENGTH, CONSENT_UUID_URL)
 
+import requests
+import json
+import logging
 import voluptuous as vol
 
 
 DATA_SCHEMA = {vol.Required("api_token"): str}
 
+_LOGGER = logging.getLogger(__name__)
 
-async def validate_api_token(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
-    """Validate the user input."""
 
-    if len(data['api_token']) != 40:
-        raise vol.Invalid("API token must be exactly 40 characters")
+async def validate_api_token(data: dict[str, Any]) -> dict[str, Any]:
+    """Validate the api token provided."""
+
+    print(data['api_token'])
+    if len(data['api_token']) != API_TOKEN_LENGTH:
+        raise vol.Invalid(
+            "API token must be exactly {} characters".format(API_TOKEN_LENGTH))
     if not data['api_token'].isalnum():
         raise vol.Invalid("API token must only contain letters and numbers")
 
     return {"title": "testing"}
+
+
+async def async_get_consent_uuid(hass: HomeAssistant, api_token: str) -> str:
+
+    session = async_get_clientsession(hass)
+    headers = {
+        'Authorization': f'Token {api_token}',
+        'Content-Type': 'application/json'
+    }
+    api_url = CONSENT_UUID_URL
+
+    async def call_api():
+        async with session.get(api_url, headers=headers) as response:
+            response_data = await response.json()
+            if response.status != 200:
+                _LOGGER.debug("Failed to fetch from the Powershaper API, response status: {} ".format(
+                    response.status))
+                raise Exception(
+                    f"API request failed with status code {response.status}")
+            return response_data[0]['consent_uuid']
+
+    return await call_api()
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -29,9 +59,10 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             # Validate the API token here
             try:
-                info = await validate_api_token(self.hass, user_input)
-
-                return self.async_create_entry(title=info["title"], data=user_input)
+                # info = await validate_api_token(self.hass, user_input)
+                consent_uuid = await async_get_consent_uuid(self.hass, user_input['api_token'])
+                _LOGGER.debug("consent_uuid: {}".format(consent_uuid))
+                return self.async_create_entry(title="powershaper", data=consent_uuid)
             except ValueError:
                 errors["base"] = "invalid_api_token"
         else:
--- const.py
+++ const.py
@@ -1,3 +1,5 @@
 """Constants for the Powershaper integration."""
 
 DOMAIN = "powershaper"
+API_TOKEN_LENGTH = 40
+CONSENT_UUID_URL = "https://app.powershaper.io/meters/api/v1/meters/"
