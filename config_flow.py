19c1bbb4a4b6bd88c00d0db40be55925de33ae3f
MemorySounds <r.sahakyan1@gmail.com>
2023-04-24 18:05:46 +0200

    cleaned-up some of the structure & logic

--- config_flow.py
+++ config_flow.py
@@ -5,8 +5,6 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from .const import (DOMAIN, API_TOKEN_LENGTH, CONSENT_UUID_URL)
 
-import requests
-import json
 import logging
 import voluptuous as vol
 
@@ -16,39 +14,34 @@ DATA_SCHEMA = {vol.Required("api_token"): str}
 _LOGGER = logging.getLogger(__name__)
 
 
-async def validate_api_token(data: dict[str, Any]) -> dict[str, Any]:
-    """Validate the api token provided."""
-
-    print(data['api_token'])
-    if len(data['api_token']) != API_TOKEN_LENGTH:
-        raise vol.Invalid(
-            "API token must be exactly {} characters".format(API_TOKEN_LENGTH))
-    if not data['api_token'].isalnum():
-        raise vol.Invalid("API token must only contain letters and numbers")
-
-    return {"title": "testing"}
-
-
 async def async_get_consent_uuid(hass: HomeAssistant, api_token: str) -> str:
 
+    if len(api_token) != API_TOKEN_LENGTH:
+        _LOGGER.debug(
+            f"Wrong token length. Expected: {API_TOKEN_LENGTH} | Received: {len(api_token)}")
+        raise ValueError
+
     session = async_get_clientsession(hass)
+    api_url = CONSENT_UUID_URL
     headers = {
         'Authorization': f'Token {api_token}',
         'Content-Type': 'application/json'
     }
-    api_url = CONSENT_UUID_URL
 
-    async def call_api():
+    try:
         async with session.get(api_url, headers=headers) as response:
             response_data = await response.json()
-            if response.status != 200:
-                _LOGGER.debug("Failed to fetch from the Powershaper API, response status: {} ".format(
-                    response.status))
-                raise Exception(
-                    f"API request failed with status code {response.status}")
-            return response_data[0]['consent_uuid']
+    except Exception as e:
+        _LOGGER.debug(
+            f"Failed to fetch data from the Powershaper API: {e}")
 
-    return await call_api()
+    if response.status != 200:
+        _LOGGER.debug("Failed to fetch from the Powershaper API, response status: {} ".format(
+            response.status))
+        raise Exception(
+            f"API request failed with status code {response.status}")
+
+    return response_data[0]['consent_uuid']
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -57,17 +50,16 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         errors = {}
         if user_input is not None:
-            # Validate the API token here
             try:
-                # info = await validate_api_token(self.hass, user_input)
                 consent_uuid = await async_get_consent_uuid(self.hass, user_input['api_token'])
                 _LOGGER.debug("consent_uuid: {}".format(consent_uuid))
-                return self.async_create_entry(title="powershaper", data=consent_uuid)
             except ValueError:
-                errors["base"] = "invalid_api_token"
-        else:
-            # Show the form to the user
-            return self.async_show_form(step_id="user",
-                                        data_schema=vol.Schema(DATA_SCHEMA),
-                                        errors=errors
-                                        )
+                errors["base"] = "invalid_token_length"
+            else:
+                return self.async_create_entry(title="powershaper", data=consent_uuid)
+
+        # Show the form to the user
+        return self.async_show_form(step_id="user",
+                                    data_schema=vol.Schema(DATA_SCHEMA),
+                                    errors=errors
+                                    )
