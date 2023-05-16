cd32da7fb2c2c8b41d7ac49eb56ae6aca34128f9
MemorySounds <r.sahakyan1@gmail.com>
2023-05-16 18:21:33 +0200

    added carbon sensor for electricity and expanded data aggregator for carbon

--- const.py
+++ const.py
@@ -7,6 +7,7 @@ POWERSHAPER_BASE_SENSOR_URL = "https://app.powershaper.io/meters/api/v1/meter/"
 
 ICON_GAS_METER = "mdi:meter-gas"
 ICON_ELECTRICITY_METER = "mdi:meter-electric"
+ICON_MOLECULE_CO2 = "mdi:molecule-co2"
 
 SENSOR_TYPE_GAS = "gas"
 SENSOR_TYPE_ELECTRICITY = "electricity"
--- sensor.py
+++ sensor.py
@@ -2,12 +2,19 @@ import datetime
 import logging
 import random
 import time
-
 import pytz
+from .const import (DOMAIN,
+                    POWERSHAPER_AUTH_URL,
+                    POWERSHAPER_BASE_SENSOR_URL,
+                    ICON_GAS_METER,
+                    ICON_ELECTRICITY_METER,
+                    ICON_MOLECULE_CO2,
+                    SENSOR_TYPE_GAS,
+                    SENSOR_TYPE_ELECTRICITY,
+                    SENSOR_TYPE_CARBON,
+                    AGGREGATE_TYPE)
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from aiohttp.client_exceptions import ClientError
-from .const import (DOMAIN, POWERSHAPER_AUTH_URL,
-                    POWERSHAPER_BASE_SENSOR_URL, ICON_GAS_METER, ICON_ELECTRICITY_METER, SENSOR_TYPE_GAS, SENSOR_TYPE_ELECTRICITY, SENSOR_TYPE_CARBON, AGGREGATE_TYPE)
 from homeassistant.const import DEVICE_CLASS_GAS, UnitOfEnergy
 from homeassistant.components.recorder.models import StatisticData
 from homeassistant.components.recorder.statistics import async_import_statistics
@@ -72,9 +79,12 @@ async def async_setup_entry(hass, entry, async_add_entities):
                          consent_uuid, api_token)
     electricity_meter = ElectricityMeter(entry.data, SENSOR_TYPE_ELECTRICITY,
                                          consent_uuid, api_token)
+    electricity_co2_meter = ElectricityCo2Emissions(entry.data, SENSOR_TYPE_CARBON,
+                                                    consent_uuid, api_token)
 
     entities.append(gas_meter)
     entities.append(electricity_meter)
+    entities.append(electricity_co2_meter)
 
     # Add the sensors to Home Assistant
     async_add_entities(entities, update_before_add=True)
@@ -89,14 +99,14 @@ async def async_setup_entry(hass, entry, async_add_entities):
     return True
 
 
-async def async_aggregate_data(data_points):
+async def async_aggregate_data(data_points, key_type):
     """Aggregate the data to hourly values, since this is the smallest unit supported in the statistics"""
     aggregated_data_points = []
     hour_energy = 0
     prev_hour = None
 
     for data_point in data_points:
-        energy_kwh = data_point['energy_kwh']
+        energy_kwh = data_point[key_type]
         time_str = data_point['time']
         time = datetime.datetime.strptime(
             time_str, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=pytz.UTC)
@@ -106,7 +116,7 @@ async def async_aggregate_data(data_points):
 
         if time.hour != prev_hour.hour:
             aggregated_data_points.append(
-                {'time': prev_hour, 'energy_kwh': hour_energy})
+                {'time': prev_hour, key_type: hour_energy})
             hour_energy = 0
             prev_hour = time.replace(minute=0, second=0, microsecond=0)
 
@@ -114,7 +124,7 @@ async def async_aggregate_data(data_points):
 
     # Append last hour
     aggregated_data_points.append(
-        {'time': prev_hour, 'energy_kwh': hour_energy})
+        {'time': prev_hour, key_type: hour_energy})
 
     return aggregated_data_points
 
@@ -122,7 +132,6 @@ async def async_aggregate_data(data_points):
 async def async_import_historic_data(hass, api_token, consent_uuid, sensor: SensorEntity, current_sum):
 
     statistics = []
-    current_state = 0
 
     metadata = {
         "has_mean": False,
@@ -137,32 +146,41 @@ async def async_import_historic_data(hass, api_token, consent_uuid, sensor: Sens
     test_date_start = datetime.datetime.strptime('2023-01-01', '%Y-%m-%d')
     test_date_end = datetime.datetime.strptime('2023-05-01', '%Y-%m-%d')
 
-    api_url = url_builder(
-        sensor._sensor_type, consent_uuid, test_date_start, test_date_end, "none")
+    # currently only retrieving the carbon_kg from the electricity meter
+    if sensor._sensor_type is SENSOR_TYPE_CARBON:
+        api_url = url_builder(
+            SENSOR_TYPE_ELECTRICITY, consent_uuid, test_date_start, test_date_end, AGGREGATE_TYPE)
+    else:
+        api_url = url_builder(
+            sensor._sensor_type, consent_uuid, test_date_start, test_date_end, AGGREGATE_TYPE)
 
     # fetch historic data
     start_fetch = time.process_time()
     historic_data = await async_fetch_data(hass, api_token, api_url)
     _LOGGER.debug(f"time to fetch data: {time.process_time() - start_fetch}")
 
+    if sensor._sensor_type is SENSOR_TYPE_CARBON:
+        key_type = 'carbon_kg'
+    else:
+        key_type = 'energy_kwh'
+
     start_aggregate = time.process_time()
-    aggregated_data = await async_aggregate_data(historic_data)
+    aggregated_data = await async_aggregate_data(historic_data, key_type)
     _LOGGER.debug(
         f"time to aggregate data: {time.process_time() - start_aggregate}")
 
     start = time.process_time()
 
     for data_point in aggregated_data:
-        current_sum += data_point['energy_kwh']
+        current_sum += data_point[key_type]
         statistics.append(
             StatisticData(
                 start=data_point['time'],
-                state=data_point['energy_kwh'],
+                state=data_point[key_type],
                 sum=current_sum,
                 last_reset=None
             )
         )
-        current_state = data_point['energy_kwh']
 
     _LOGGER.debug(f"time to add statistics: {time.process_time() - start}")
 
@@ -170,7 +188,7 @@ async def async_import_historic_data(hass, api_token, consent_uuid, sensor: Sens
     async_import_statistics(hass, metadata, statistics)
     _LOGGER.debug("Successfully imported statistics")
 
-    return current_state
+    return current_sum
 
 
 def url_builder(sensor_type: str, consent_uuid: str, start_date: datetime, end_date: datetime, aggregate: str) -> str:
@@ -208,7 +226,7 @@ class GasMeter(SensorEntity):
             # Set the state of the sensor
 
             _LOGGER.debug(f"current_state (gas): {current_state}")
-            self._state = current_state
+            # self._state = 0
 
         except Exception as error:
             _LOGGER.error("Error fetching data: %s", error)
@@ -263,7 +281,7 @@ class ElectricityMeter(SensorEntity):
             _LOGGER.debug("Successfully imported electricity data")
 
             _LOGGER.debug(f"current_state (elec): {current_state}")
-            self._state = current_state
+            # self._state = 0
         except Exception as error:
             _LOGGER.error("Error fetching data: %s", error)
 
@@ -286,6 +304,60 @@ class ElectricityMeter(SensorEntity):
     def icon(self):
         """Return the icon of the sensor."""
         return ICON_ELECTRICITY_METER
+
+    @property
+    def device_class(self):
+        """Return the device class of the sensor."""
+        return self._attr_device_class
+
+
+class ElectricityCo2Emissions(SensorEntity):
+    """Representation of an electricity carbon sensor."""
+
+    _attr_device_class = SensorDeviceClass.ENERGY
+    _attr_unit_of_measurement = UnitOfEnergy.KILO_WATT_HOUR
+    _attr_state_class = SensorStateClass.TOTAL_INCREASING
+
+    def __init__(self, entry_data, sensor_type,  consent_uuid, api_token):
+        """Initialize a gas sensor."""
+        self._attr_unique_id = "powershaper"+sensor_type+"123"
+        self._state = None
+        self._entry_data = entry_data
+        self._sensor_type = sensor_type
+        self._consent_uuid = consent_uuid
+        self._api_token = api_token
+
+    async def async_update(self):
+        """Fetch the latest data from the API."""
+        try:
+            _LOGGER.debug("polling carbon co2 endpoint")
+            current_state = await async_import_historic_data(self.hass, self._api_token, self._consent_uuid, self, 0)
+            _LOGGER.debug("Successfully imported co2 data")
+
+            _LOGGER.debug(f"current_state (co)): {current_state}")
+            # self._state = 0
+        except Exception as error:
+            _LOGGER.error("Error fetching data: %s", error)
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return f"{self._sensor_type}"
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return self._attr_unit_of_measurement
+
+    @property
+    def icon(self):
+        """Return the icon of the sensor."""
+        return ICON_MOLECULE_CO2
 
     @property
     def device_class(self):
